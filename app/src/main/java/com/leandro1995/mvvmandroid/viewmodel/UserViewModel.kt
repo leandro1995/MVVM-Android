db3ba7213e3e2e69dc6e23684ff69e1222b6360f
Leandro Castillo Borja <leccbo1995@gmail.com>
2019-12-04 11:17:09 -0500

    Implementar viewmodel en login

--- app/src/main/java/com/leandro1995/mvvmandroid/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/leandro1995/mvvmandroid/viewmodel/UserViewModel.kt
@@ -2,8 +2,9 @@ package com.leandro1995.mvvmandroid.viewmodel
 
 import android.content.Intent
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-class UserViewModel {
+class UserViewModel : ViewModel() {
 
     val activityMutable: MutableLiveData<Intent> by lazy {
         MutableLiveData<Intent>()
