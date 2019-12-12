14f3bfb0f32cd4150940fea18c3277f86137da39
Leandro Castillo Borja <leccbo1995@gmail.com>
2019-12-12 11:22:32 -0500

    Validar login

--- app/src/main/java/com/leandro1995/mvvmandroid/MainActivity.kt
+++ app/src/main/java/com/leandro1995/mvvmandroid/MainActivity.kt
@@ -2,7 +2,9 @@ package com.leandro1995.mvvmandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.leandro1995.mvvmandroid.databinding.ActivityMainBinding
 import com.leandro1995.mvvmandroid.viewmodel.UserViewModel
@@ -20,5 +22,17 @@ class MainActivity : AppCompatActivity() {
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mainBinding.userModel = userViewModel
         mainBinding.executePendingBindings()
+
+        observer()
+    }
+
+    private fun observer() {
+        userViewModel.activityMutable.observe(this, Observer {
+            
+        })
+
+        userViewModel.messageMutable.observe(this, Observer {
+            Toast.makeText(this, getString(it), Toast.LENGTH_SHORT).show()
+        })
     }
 }
--- app/src/main/java/com/leandro1995/mvvmandroid/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/leandro1995/mvvmandroid/viewmodel/UserViewModel.kt
@@ -1,31 +1,27 @@
 package com.leandro1995.mvvmandroid.viewmodel
 
-import android.content.Intent
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.leandro1995.mvvmandroid.R
 
 class UserViewModel : ViewModel() {
 
-    val activityMutable: MutableLiveData<Intent> by lazy {
-        MutableLiveData<Intent>()
+    val activityMutable: MutableLiveData<Pair<String, String>> by lazy {
+        MutableLiveData<Pair<String, String>>()
     }
 
-    val messageMutable:MutableLiveData<String> by lazy {
-        MutableLiveData<String>()
+    val messageMutable: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
     }
 
     var user = ""
     var password = ""
 
-    fun loginUser()
-    {
-        if(user == "leandro" && password == "12345678")
-        {
-
-        }
-        else
-        {
-            messageMutable.value = "Usuario o contraseña no son correctas"
+    fun loginUser() {
+        if (user == "leandro" && password == "12345678") {
+            activityMutable.value = Pair("Leandro", "Castillo")
+        } else {
+            messageMutable.value = R.string.verify_user_message
         }
     }
 }
